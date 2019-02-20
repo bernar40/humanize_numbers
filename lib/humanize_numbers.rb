30e0ec7a886da29be306a36fab1484be21ff524c
Bernardo Ruga <bernardo.ruga@hotmail.com>
2019-02-20 13:01:01 -0300

    Update Humanize to spanish only

--- lib/humanize_numbers.rb
+++ lib/humanize_numbers.rb
@@ -16,11 +16,13 @@ module HumanizeNumbers
       def humanize
 
         words = []
-        quantities_array = %w[ ~ ~ milli ~ billi ~ trilli ~ ]
+        quantities_array_es = %w[ ~ ~ milli ~ billi ~ trilli ~ ]
         number = self.to_f
+        conector = "con"
+        zero_string = "cero"
 
         if number.to_i == 0
-          words << self.zero_string
+          words << zero_string
         else
           decimal = number.to_s.split(".")[1].nil? ? 0 : number.to_s.split(".")[1]
           number = number.to_s.split(".")[0]
@@ -35,13 +37,9 @@ module HumanizeNumbers
             if groups[number].to_i > 0
               case number
               when 1,3,5,7,9
-                words << "thousand" if I18n.locale == :en
                 words << "mil" if I18n.locale == :es
-                words << "mil" if I18n.locale == :pt
               else
-                words << (groups[number].reverse.to_i > 1 ? "#{self.quantities[number]}" : "#{self.quantities[number]}") if I18n.locale == :en
-                words << (groups[number].reverse.to_i > 1 ? "#{self.quantities[number]}" : "#{self.quantities[number]}") if I18n.locale == :pt
-                words << (groups[number].reverse.to_i > 1 ? "#{quantities_array[number]}ones" : "#{quantities_array[number]}ón") if I18n.locale == :es
+                words << (groups[number].reverse.to_i > 1 ? "#{quantities_array_es[number]}ones" : "#{quantities_array_es[number]}ón")
               end
               words << number_to_words(groups[number].reverse)
             end
@@ -52,90 +50,6 @@ module HumanizeNumbers
 
       protected
 
-      def conector
-        case I18n.locale
-        when :en
-          "with"
-        when :es
-          "con"
-        when :pt
-          "com"
-        end
-      end
-
-      def and_string
-        return "y" if I18n.locale == :es
-        return "e" if I18n.locale == :pt
-      end
-
-      def zero_string
-        case I18n.locale
-        when :en
-          "zero"
-        when :es
-          "cero"
-        end
-      end
-
-      def units
-        # 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-        case I18n.locale
-        when :en
-          %w[ ~ one two three four five six seven eight nine ]
-        when :es
-          %w[ ~ un dos tres cuatro cinco seis siete ocho nueve ]
-        when :pt
-          %w[ ~ um dois três quatro cinco seis sete oito nove ]
-        end
-      end
-
-      def tens
-        # 10, 20, 30, 40, 50, 60, 70, 80, 90
-        case I18n.locale
-        when :en
-          %w[ ~ ten twenty thirty forty fifty sixty seventy eighty ninety ]
-        when :es
-          %w[ ~ diez veinte treinta cuarenta cincuenta sesenta setenta ochenta noventa ]
-        when :pt
-          %w[ ~ dez veinte trinta quarenta cinquenta sessenta setenta oitenta noventa ]
-        end
-      end
-
-      def hundreds
-        # 100, 200, 300, 400 , 500, 600, 700, 800, 900
-        case I18n.locale
-        when :en
-          [ nil, 'one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred' ]
-        when :es
-          %w[ cien ciento doscientos trescientos cuatrocientos quinientos seiscientos setecientos ochocientos novecientos ]
-        when :pt
-          %w[ ~ cem duzentos trezentos quatrocentos quinhentos seiscentos setecentos oitocentos novecentos ]
-        end
-      end
-
-      def teens
-        # 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
-        case I18n.locale
-        when :en
-          %w[ ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen ]
-        when :es
-          %w[ diez once doce trece catorce quince dieciseis diecisiete dieciocho diecinueve ]
-        when :pt
-          %W[ dez onze doze treze catorze quinze dezesseis dezessete dezoito dezenove ]
-        end
-      end
-
-      def quantities
-        case I18n.locale
-        when :en
-          %w[ ~ ~ mill ~ bill ~ trill ~ cuatrill ~ quintill ~ ]
-        when :es
-          %w[ ~ ~ milli ~ billi ~ trilli ~ ]
-        when :pt
-          %[ ~ ~ milhão ~ bilhão ~ trilhão ~  mil bilhões ~ quintilhões ]
-        end
-      end
-
       def number_to_words(number)
 
         hundreds = number[0,1].to_i
@@ -145,7 +59,7 @@ module HumanizeNumbers
         units_array = %w[ ~ un dos tres cuatro cinco seis siete ocho nueve ]
         teens_array = %w[ diez once doce trece catorce quince dieciseis diecisiete dieciocho diecinueve ]
         tens_array = %w[ ~ diez veinte treinta cuarenta cincuenta sesenta setenta ochenta noventa ]
-
+        and_string = "y"
         text = []
 
         if hundreds > 0
@@ -163,11 +77,8 @@ module HumanizeNumbers
           when 1
             text << (units == 0 ? tens_array[tens] : teens_array[units])
           when 2
-            text << (units == 0 ? tens_array[tens] : "veinti#{units_array[units]}") if I18n.locale == :es
-            text << self.tens[tens] if I18n.locale == :en
-            text <<  "#{self.and_string} #{self.tens[tens]}" if I18n.locale == :pt
+            text << (units == 0 ? tens_array[tens] : "veinti#{units_array[units]}")
           else
-            text << self.and_string if (I18n.locale == :pt and hundreds > 0)
             text << tens_array[tens]
           end
         end
@@ -176,12 +87,7 @@ module HumanizeNumbers
           if tens == 0
             text << units_array[units]
           elsif tens > 2
-            text << "#{self.and_string} #{units_array[units]}" if I18n.locale == :es
-            text << "#{self.and_string} #{self.units[units]}" if I18n.locale == :pt
-            text << self.units[units] if I18n.locale == :en
-          else
-            text << "#{self.and_string} #{self.units[units]}" if I18n.locale == :pt
-            text << self.units[units] if I18n.locale == :en
+            text << "#{and_string} #{units_array[units]}"
           end
         end
         return text.join(' ')
